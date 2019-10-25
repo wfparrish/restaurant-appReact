1b1f1bf46405df6200bffa1beaf771258966d863
williamparrish <williparrish@gmail.com>
2019-10-24 22:21:07 -0600

    musta dun sumpin

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,7 +4,7 @@ import classes from "./Layout.module.css";
 
 const layout = props => (
   <Auxiliary>
-    <div>Toolbar, SideDrawer, Backdrop</div>
+    {/* <div>Toolbar, SideDrawer, Backdrop</div> */}
     <main className={classes.Content}>{props.children}</main>
   </Auxiliary>
 );
